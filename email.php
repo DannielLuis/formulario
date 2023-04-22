ffe2445cc9cac2630cd05a7e110453d487b858bc
Daniel Luis <danniellcontato@gmail.com>
2023-04-22 05:57:07 -0300

    Primeiro commit

---
+++ email.php
@@ -0,0 +1,29 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Destinatario
+  $destino = "danielnetompc@gmail.com";
+  $assunto = "Testando envio de email";
+
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=http://localhost/Enviar_e-mail_PHP/index.html'>";
+?>
